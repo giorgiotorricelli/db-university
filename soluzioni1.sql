793ad788e5767bf66b730d10404917b6c0368cdb
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-06-04 15:52:14 +0200

    soluzione 3

--- soluzioni1.sql
+++ soluzioni1.sql
@@ -9,3 +9,10 @@ WHERE year(`date_of_birth`) LIKE 1990;
 SELECT *
 FROM `courses`
 WHERE `courses`.`cfu` > 10;
+
+-- soluzione 3
+
+SELECT *
+FROM `students`
+WHERE TIMESTAMPDIFF(YEAR, `date_of_birth`, CURDATE()) > 30
+ORDER BY `date_of_birth` DESC;
