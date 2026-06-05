3abf6a1b263d64e07aa98067586a60fde7fdd19e
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-06-05 16:27:13 +0200

    seconda soluzione

--- soluzioni2.sql
+++ soluzioni2.sql
@@ -8,3 +8,13 @@ SELECT
 FROM students
 GROUP BY YEAR(enrolment_date)
 ORDER BY enrolment_date;
+
+-- seconda soluzione 
+
+SELECT SUM(conteggio) AS totale_insegnanti_in_coabitazione
+FROM (
+    SELECT COUNT(*) AS conteggio
+    FROM teachers
+    GROUP BY office_address
+    HAVING COUNT(*) > 1
+) AS uffici_filtrati;
